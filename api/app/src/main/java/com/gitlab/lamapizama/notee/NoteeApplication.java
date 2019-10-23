1e556abf6ea231f1d32d7e5eaca6bd50bb57c015
Rafał Staśkiewicz <rstaskiewicz@e-point.pl>
2019-10-23 18:09:01 +0200

    [api] Add the basic gradle project configuration

---
+++ api/app/src/main/java/com/gitlab/lamapizama/notee/NoteeApplication.java
@@ -0,0 +1,13 @@
+package com.gitlab.lamapizama.notee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteeApplication.class, args);
+    }
+
+}
