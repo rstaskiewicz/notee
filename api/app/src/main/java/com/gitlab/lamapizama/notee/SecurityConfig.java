124c9c9e01e7afb9389699ce8efde67572629991
Rafał Staśkiewicz <rstaskiewicz@e-point.pl>
2020-01-21 14:41:38 +0100

    [api] Fix cors

--- api/app/src/main/java/com/gitlab/lamapizama/notee/SecurityConfig.java
+++ api/app/src/main/java/com/gitlab/lamapizama/notee/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableResourceServer
@@ -28,7 +30,7 @@ class SecurityConfig extends ResourceServerConfigurerAdapter {
                         "/webjars/**")
                 .permitAll()
                 .anyRequest().authenticated()
-                .and().cors().disable();
+                .and().cors();
     }
 
     @Bean
@@ -38,5 +40,17 @@ class SecurityConfig extends ResourceServerConfigurerAdapter {
         tokenService.setClientId(properties.getAuth().getJsClient().getClientId());
         tokenService.setClientSecret(properties.getAuth().getJsClient().getClientSecret());
         return tokenService;
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000");
+            }
+        };
     }
 }
